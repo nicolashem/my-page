1afdaa7314da7d0d55b86e173695dfe6492c9c88
nicolash1989 <91009364+nicolash1989@users.noreply.github.com>
2021-11-29 15:44:49 +0100

    commit

--- svg/random.js
+++ svg/random.js
@@ -1,4 +1,5 @@
 
+
 //CODE
 let twrk = {};
 
@@ -17,13 +18,21 @@ twrk.scale = function ({ width, height }) {
 }
 twrk.scale({ height: 120 });
 
+
 //SVG
 let svg = {};
 
 svg.nameSpace = "http://www.w3.org/2000/svg";
 
-svg.dot = function (io) {
-    return "M " + io.x * twrk.res + " " + io.y * twrk.res + " z";
+svg.path = function (ia) {
+    
+    let output = "M ";
+    for (var i = 0; i < ia.length; i++) {
+        output += ia[i].x * twrk.res + " " + ia[i].y * twrk.res + " ";
+    }
+    output += "z";
+    
+    return output;
 };
 
 svg.makeLayer = function ({ parent, id, x = 0, y = 0 }) {
@@ -33,7 +42,7 @@ svg.makeLayer = function ({ parent, id, x = 0, y = 0 }) {
     parent.appendChild(dom[id]);
 };
 
-svg.makeLine = function ({ parent, id, d = "", color = "#ffffff", stroke = 1, cap = "butt", join = "round" }) {
+svg.makeLine = function ({ parent, id, d = "", color = "#FFFFFF", stroke = 1, cap = "butt", join = "round" }) {
     dom[id] = document.createElementNS(svg.nameSpace, "path");
     dom[id].setAttributeNS(null, "fill", "none");
     dom[id].setAttributeNS(null, "d", d);
@@ -44,6 +53,15 @@ svg.makeLine = function ({ parent, id, d = "", color = "#ffffff", stroke = 1, ca
     dom[id].id = id;
     parent.appendChild(dom[id]);
 };
+
+svg.makeShape = function ({ parent, id, d = "", color = "#ff00ff" }) {
+    dom[id] = document.createElementNS(svg.nameSpace, "path");
+    dom[id].setAttributeNS(null, "fill", color);
+    dom[id].setAttributeNS(null, "d", d);
+    dom[id].id = id;
+    parent.appendChild(dom[id]);
+};
+
 
 //DOM
 let dom = {};
@@ -57,56 +75,31 @@ document.body.appendChild(dom.stage);
 //svg layer
 svg.makeLayer({ parent: dom.stage, id: "svgLayer", x: 0, y: 0 });
 
-let simplex = new SimplexNoise();
+let square = 20;
+let distance = 10;
+let cells = 6;
+let offset = -cells/2 * distance
+let sizes = [square, square * 0.6, square * 0.3];
 
-//drawing
-let step = 1.8;
-let position = { x: -50, y: -50 };
-
-let resolution = 0.05;
-let amplification = 2;
-let amplificationSize = 2.9;
-
-let strokeW = 0.01
-let strokeVar = + 0.001
-
-let colorsArray = ["#DF4A4A", "#6495ED", "#00FFFF", "#1E90FF", "#ADD8E6"]
-
-svg.makeLine({
-    parent: dom.svgLayer,
-    id: "dots", 
-    cap: "round", 
-    stroke: 0, 
-    color: "#176DC8", 
-    d: ""
-});
-
-function loop(zeit){
-    
-    let dotsArray = "";
-    let numDots = 40
-    
-    for(let y = 0; y < numDots; y++) {
+//nested loop
+for(let x = 0; x < cells; x++){
+    for(let y = 0; y < cells; y++){
         
-        for(let x = 0; x < numDots; x++) {
-            
-            let noiseX = simplex.noise2D(x * resolution + zeit/4000, y * resolution) * amplification;
-            let noiseY = simplex.noise2D(y * resolution + zeit/4000+10, x * resolution) * amplification;
-
-            dotsArray += svg.dot({x: position.x + x * step + noiseX, y: position.y + y * step + noiseY});
-            
-            dom["dots"].setAttributeNS(null, "stroke-width", strokeW);
+        let xpos = offset + x * distance
+        let ypos = offset + y * distance
         
-            strokeW = simplex.noise2D(x * resolution + zeit/2000, y * resolution) * amplificationSize + 9;
-            
-        }
+        let dice = Math.ceil(Math.random() * 6);
+        let step = square / 5
+        
+        let path = [
+            {x: xpos - square / 2 + Math.ceil(Math.random() * 6) * step, y: ypos - square / 2 },
+            {x: xpos + square / 2, y: ypos + square / 2 + Math.ceil(Math.random() * 6) * step },
+            {x: xpos + square / 2 - Math.ceil(Math.random() * 6) * step, y: ypos + square / 2 },
+            {x: xpos - square / 2, y: ypos + square / 2 - Math.ceil(Math.random() * 6) * step }
+        ];
+        
+        svg.makeLine({parent: dom.svgLayer, id: "punkt", cap: "round", stroke: 0.2, color: "#000", d: svg.path(path)})
+        
     }
-    
-
-    dom["dots"].setAttribute("d", dotsArray);
-    
-    requestAnimationFrame(loop);
-};
-
-loop(0);
+}
 
